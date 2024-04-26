812104b52627f72050d66a1258bff8ec0a3ba357
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-04-26 11:09:58 -0500

    updated JS to include a reset

--- script.js
+++ script.js
@@ -11,25 +11,28 @@ function processInput() {
   // retrieves the HTML element for labelQuestion
   const output = document.getElementById("output");
   // retrieves the HTML element for output
+  const btnClear = document.getElementById("btnClear");
 
   if (currentStep === 0) {
     // if currentStep is 0, function stores the user's name, ask the next question, age
     userData.name = userInput.value;
     labelQuestion.textContent = "How old are you?";
-    userInput.value = "";
     userInput.placeholder = "Enter your age";
+    userInput.value = "";
   } else if (currentStep === 1) {
     // if currentStep is 1, function stores the user's age then ask about location
     userData.age = userInput.value;
     labelQuestion.textContent = "Where do you live?";
-    userInput.value = "";
     userInput.placeholder = "Type your location";
+    userInput.value = "";
   } else if (currentStep === 2) {
     // if currentStep is 2, the function will store the user's location, function then returns (which ends the function)
     userData.location = userInput.value;
     userInput.style.display = "none";
+    labelQuestion.style.display = "none";
     document.querySelector("button").style.display = "none";
     output.textContent = `Hi ${userData.name}, I see that you're ${userData.age} years old and live in ${userData.location}.`;
+    btnClear.style.display = "block";
     return;
   }
   currentStep++;
@@ -43,8 +46,19 @@ document
       processInput();
     }
   });
-// attaches an event listerner to the user input field that triggers the processInput function when the Enter key is pressed.
 
-// next steps
+// add reset
 
-// add button to clear the input to start over again
+btnClear.addEventListener("click", function () {
+  // Reset user data and form
+  userData = {};
+  currentStep = 0;
+  userInput.style.display = "block";
+  labelQuestion.style.display = "block";
+  document.querySelector("button").style.display = "block";
+  labelQuestion.textContent = "What is your name?";
+  userInput.placeholder = "Enter name here";
+  userInput.value = "";
+  output.textContent = "";
+  btnClear.style.display = "none"; // Hides the reset button
+});
