d3d578e92a6764bc65a52fe4f805c98d2571b3c9
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-05-03 11:25:09 -0500

    using Netlify CLI created function

--- netlify/functions/swapi.js
+++
@@ -1,9 +0,0 @@
-// temp JS to ensure it's working:
-// https://www.netlify.com/blog/intro-to-serverless-functions/
-
-export const handler: Handler = async (event, context) => {
-  return {
-    body: JSON.stringify({ message: "Hello World" }),
-    statusCode: 200,
-  };
-};
