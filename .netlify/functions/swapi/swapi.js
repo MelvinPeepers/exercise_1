8585d9b172464df91a4971413d86cda94aa709d8
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-05-07 12:11:50 -0500

    added console.time to try out

--- .netlify/functions/swapi/swapi.js
+++ .netlify/functions/swapi/swapi.js
@@ -1,5 +1,19 @@
 // https://docs.netlify.com/functions/get-started/?fn-language=js#code-your-function-2
 
-export default async (req, context) => {
-  return new Response("Hello, world!");
-};
+export async function handler(event, context) {
+  console.time("Start");
+
+  // Perform an asynchronous operation (e.g., setTimeout)
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000); // Wait for 1 second
+  });
+
+  console.timeEnd("End");
+
+  return {
+    statusCode: 200,
+    body: "Netlify function executed!",
+  };
+}
