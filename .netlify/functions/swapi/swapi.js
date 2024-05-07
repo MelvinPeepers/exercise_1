a9ebd3bc9b17db94e6fe3fcd40a094b38d54a0e3
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-05-07 12:24:02 -0500

    added console.time

--- .netlify/functions/swapi/swapi.js
+++ .netlify/functions/swapi/swapi.js
@@ -1,7 +1,7 @@
 // https://docs.netlify.com/functions/get-started/?fn-language=js#code-your-function-2
 
 export async function handler(event, context) {
-  console.time("Start");
+  console.time("TIME");
 
   // Perform an asynchronous operation (e.g., setTimeout)
   await new Promise((resolve) => {
@@ -10,7 +10,7 @@ export async function handler(event, context) {
     }, 1000); // Wait for 1 second
   });
 
-  console.timeEnd("End");
+  console.timeEnd("TIME");
 
   return {
     statusCode: 200,
