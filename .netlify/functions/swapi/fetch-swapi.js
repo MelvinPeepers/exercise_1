594c8ff3efaa1e8af958c7379a454b9930add924
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-05-10 11:47:49 -0500

    changed fetch

--- .netlify/functions/swapi/fetch-swapi.js
+++ .netlify/functions/swapi/fetch-swapi.js
@@ -1,17 +1,36 @@
 const fetch = require("node-fetch");
 
-exports.handler = async function (event, context) {
+// exports.handler = async function (event, context) {
+//   try {
+//     const response = await fetch("https://www.swapi.tech/api/planets/1/");
+//     const data = await response.json();
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(data),
+//     };
+//   } catch (err) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: err.message }),
+//     };
+//   }
+// };
+
+exports.handler = async (event, context) => {
+  const API_ENDPOINT = "https://www.swapi.tech/api/planets/1/";
+
   try {
-    const response = await fetch("https://www.swapi.tech/api/planets/1/");
-    const data = await response.json();
+    const response = await fetch(API_ENDPOINT);
+    const data = response.data;
     return {
       statusCode: 200,
       body: JSON.stringify(data),
     };
-  } catch (err) {
+  } catch (error) {
+    console.error(error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: err.message }),
+      body: JSON.stringify({ error: "An error occurred" }),
     };
   }
 };
