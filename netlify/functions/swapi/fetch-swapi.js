a0d90a65935040863554f09c8fabf393e6204fcb
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-05-13 12:40:07 -0500

    added console.time to fetch function

--- netlify/functions/swapi/fetch-swapi.js
+++ netlify/functions/swapi/fetch-swapi.js
@@ -1,46 +1,13 @@
 const fetch = require("node-fetch");
 
-// exports.handler = async function (event, context) {
-//   try {
-//     const response = await fetch("https://www.swapi.tech/api/planets/1/");
-//     const data = await response.json();
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify(data),
-//     };
-//   } catch (err) {
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: err.message }),
-//     };
-//   }
-// };
-
-// exports.handler = async (event, context) => {
-//   const API_ENDPOINT = "https://www.swapi.tech/api/planets/1/";
-
-//   try {
-//     const response = await fetch(API_ENDPOINT);
-//     const data = response.data;
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify(data),
-//     };
-//   } catch (error) {
-//     console.error(error);
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: "An error occurred" }),
-//     };
-//   }
-// };
-
 exports.handler = async function (event, context) {
   try {
     const fetch = await import("node-fetch");
+    console.time("");
     const response = await fetch.default(
       "https://www.swapi.tech/api/planets/1/"
     );
+    console.timeEnd("FETCHTIME");
     const data = await response.json();
     return {
       statusCode: 200,
