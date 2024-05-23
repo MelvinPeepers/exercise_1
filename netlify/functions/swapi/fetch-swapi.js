583202c390cc0cd3f0e3f1b86aa6831cf459eb5d
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-05-23 10:27:18 -0500

    extract just the name

--- netlify/functions/swapi/fetch-swapi.js
+++ netlify/functions/swapi/fetch-swapi.js
@@ -4,15 +4,13 @@ exports.handler = async function (event, context) {
   try {
     const fetch = await import("node-fetch");
     console.time("FETCHTIME");
-    const response = await fetch.default(
-      "https://www.swapi.tech/api/planets/1"
-    );
+    const response = await fetch.default("https://www.swapi.tech/api/planets/");
     console.timeEnd("FETCHTIME");
     const data = await response.json();
-    const name = data.name;
+    const name = data.result.properties.name;
     return {
       statusCode: 200,
-      body: JSON.stringify(name),
+      body: JSON.stringify({ name }),
     };
   } catch (err) {
     return {
