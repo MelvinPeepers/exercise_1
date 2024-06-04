ee3567184740bde029b5b86bbea9f3d61c3bf1d1
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-06-04 09:58:02 -0500

    still not working

--- netlify/functions/swapi/fetch-swapi.js
+++ netlify/functions/swapi/fetch-swapi.js
@@ -3,14 +3,15 @@ const fetch = require("node-fetch");
 exports.handler = async function (event, context) {
   try {
     console.time("FETCHTIME");
-    const response = await fetch("https://www.swapi.tech/api/planets/1");
+    const response = await fetch.default(
+      "https://www.swapi.tech/api/planets/1"
+    );
     console.timeEnd("FETCHTIME");
     const data = await response.json();
-
-    const name = data.name;
+    const name = data.result.properties.name;
     return {
       statusCode: 200,
-      body: JSON.stringify(name),
+      body: JSON.stringify({ name }),
     };
   } catch (err) {
     return {
@@ -19,3 +20,47 @@ exports.handler = async function (event, context) {
     };
   }
 };
+
+// const fetch = require("node-fetch");
+
+// exports.handler = async function (event, context) {
+//   try {
+//     console.time("FETCHTIME");
+//     const response = await fetch("https://www.swapi.tech/api/planets/1");
+//     console.timeEnd("FETCHTIME");
+//     const data = await response.json();
+
+//     // Access the name directly from the data object
+//     const name = data.name;
+
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(name),
+//     };
+//   } catch (err) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: err.message }),
+//     };
+//   }
+// };
+
+// const fetch = require("node-fetch");
+
+// exports.handler = async function (event, context) {
+//   try {
+//     const response = await fetch("https://www.swapi.tech/api/planets/1");
+//     const data = await response.json();
+
+//     const name = data.name;
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(name),
+//     };
+//   } catch (err) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: err.message }),
+//     };
+//   }
+// };
