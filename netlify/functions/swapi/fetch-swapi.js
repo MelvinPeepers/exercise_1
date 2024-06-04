537490851e170c26bfae418f71377f0905436311
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2024-06-04 09:45:45 -0500

    redoing fetch

--- netlify/functions/swapi/fetch-swapi.js
+++ netlify/functions/swapi/fetch-swapi.js
@@ -2,17 +2,15 @@ const fetch = require("node-fetch");
 
 exports.handler = async function (event, context) {
   try {
-    const fetch = await import("node-fetch");
     console.time("FETCHTIME");
-    const response = await fetch.default(
-      "https://www.swapi.tech/api/planets/1"
-    );
+    const response = await fetch("https://www.swapi.tech/api/planets/1");
     console.timeEnd("FETCHTIME");
     const data = await response.json();
-    const name = data.result.properties.name;
+
+    const name = data.name;
     return {
       statusCode: 200,
-      body: JSON.stringify({ name }),
+      body: JSON.stringify(name),
     };
   } catch (err) {
     return {
